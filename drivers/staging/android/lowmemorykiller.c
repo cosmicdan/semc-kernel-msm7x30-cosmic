c3331a0d7008ab26df9ac6ea2eeb216004fea098
CosmicDan <cosmicdan@cosmicdan-vm.(none)>
2012-08-06 12:57:54 +1000

    That's no good... drop fudgeswap to 512

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -54,7 +54,7 @@ static int lowmem_minfree_size = 4;
 
 static struct task_struct *lowmem_deathpending;
 static unsigned long lowmem_deathpending_timeout;
-static int fudgeswap = 1024;
+static int fudgeswap = 512;
 
 #define lowmem_print(level, x...)			\
 	do {						\
